14c7a1ba91c22d9a125cf3067d2f6cb78937dc11
Zack McGhee <zack@zackmcghee.com>
2010-12-04 18:58:32 -0600

    Adding venue to film log admin change list view

--- clayton/movies/admin.py
+++ clayton/movies/admin.py
@@ -10,7 +10,7 @@ class VenueAdmin(admin.ModelAdmin):
 class FilmLogAdmin(admin.ModelAdmin):
     date_hierarchy = 'date'
     list_display = ('date', 'movie_title', 'release_year', 'rating',
-        'is_nonfiction', 'is_repeat_viewing',)
+        'is_nonfiction', 'is_repeat_viewing', 'venue',)
     list_display_links = ('movie_title',)
     list_filter = ('is_nonfiction', 'is_repeat_viewing', 'rating', 'venue', 'release_year',)
     search_fields = ['movie_title', 'imdb_id',]
